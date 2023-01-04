899dd1503e079affacbe5ce7b5fa1a1ff0d0929e
James Morris <8013000+RawToast@users.noreply.github.com>
2023-01-04 16:28:59 +0000

    Navigate to check registered details

--- app/models/CheckRegisteredDetails.scala
+++ app/models/CheckRegisteredDetails.scala
@@ -35,7 +35,7 @@ object CheckRegisteredDetails extends Enumerable.Implicits {
   def options(implicit messages: Messages): Seq[RadioItem] = values.zipWithIndex.map {
     case (value, index) =>
       RadioItem(
-        content = Text(messages(s"checkRegisteredDetails.${value.toString}")),
+        content = Text(messages(s"site.${value.toString}")),
         value = Some(value.toString),
         id = Some(s"value_$index")
       )
