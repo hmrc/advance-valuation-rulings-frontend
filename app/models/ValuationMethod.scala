16fda75b2cbb537f234891525de787eb4931349a
James Morris <8013000+RawToast@users.noreply.github.com>
2023-01-11 17:06:49 +0000

    remove unintended changes

--- app/models/ValuationMethod.scala
+++ app/models/ValuationMethod.scala
@@ -17,34 +17,15 @@
 package models
 
 import play.api.i18n.Messages
-import play.api.libs.json.JsError
-import play.api.libs.json.JsString
-import play.api.libs.json.JsSuccess
-import play.api.libs.json.Reads
-import play.api.libs.json.Writes
 import uk.gov.hmrc.govukfrontend.views.Aliases.Text
 import uk.gov.hmrc.govukfrontend.views.viewmodels.radios.RadioItem
 
-sealed abstract class ValuationMethod(val name: String)
+sealed trait ValuationMethod
 
-object ValuationMethod {
+object ValuationMethod extends Enumerable.Implicits {
 
-  case object Method1 extends ValuationMethod("method1")
-  case object Method2 extends ValuationMethod("method2")
-
-  implicit def reads: Reads[ValuationMethod] =
-    Reads {
-      case JsString(str) =>
-        str match {
-          case "method1" => JsSuccess(Method1)
-          case "method2" => JsSuccess(Method2)
-          case _         => JsError("error.invalid")
-        }
-      case _             => JsError("error.invalid")
-    }
-
-  implicit def writes: Writes[ValuationMethod] =
-    Writes(value => JsString(value.name))
+  case object Method1 extends WithName("method1") with ValuationMethod
+  case object Method2 extends WithName("method2") with ValuationMethod
 
   val values: Seq[ValuationMethod] = Seq(
     Method1,
@@ -54,8 +35,8 @@ object ValuationMethod {
   def options(implicit messages: Messages): Seq[RadioItem] = values.zipWithIndex.map {
     case (value, index) =>
       RadioItem(
-        content = Text(messages(s"valuationMethod.${value.name}")),
-        value = Some(value.name),
+        content = Text(messages(s"valuationMethod.${value.toString}")),
+        value = Some(value.toString),
         id = Some(s"value_$index")
       )
   }
--- test/models/ValuationMethodSpec.scala
+++ test/models/ValuationMethodSpec.scala
@@ -39,7 +39,7 @@ class ValuationMethodSpec
 
       forAll(gen) {
         valuationMethod =>
-          JsString(valuationMethod.name)
+          JsString(valuationMethod.toString)
             .validate[ValuationMethod]
             .asOpt
             .value mustEqual valuationMethod
@@ -48,7 +48,7 @@ class ValuationMethodSpec
 
     "must fail to deserialise invalid values" in {
 
-      val gen = arbitrary[String] suchThat (!ValuationMethod.values.map(_.name).contains(_))
+      val gen = arbitrary[String] suchThat (!ValuationMethod.values.map(_.toString).contains(_))
 
       forAll(gen) {
         invalidValue =>
@@ -61,7 +61,7 @@ class ValuationMethodSpec
       val gen = Gen.oneOf(ValuationMethod.values.toSeq)
 
       forAll(gen) {
-        valuationMethod => Json.toJson(valuationMethod) mustEqual JsString(valuationMethod.name)
+        valuationMethod => Json.toJson(valuationMethod) mustEqual JsString(valuationMethod.toString)
       }
     }
   }
