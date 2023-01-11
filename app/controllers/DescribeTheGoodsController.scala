945966d541ea63acc939a97b33094ee1488a9801
carl.thompson <carl.thompson@opencastsoftware.com>
2023-01-11 11:08:58 +0000

    text area wip

--- app/controllers/DescribeTheGoodsController.scala
+++ app/controllers/DescribeTheGoodsController.scala
@@ -17,18 +17,15 @@
 package controllers
 
 import javax.inject.Inject
-
 import scala.concurrent.{ExecutionContext, Future}
-
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
-
 import controllers.actions._
 import forms.DescribeTheGoodsFormProvider
 import models.Mode
 import navigation.Navigator
-import pages.DescribeTheGoodsPage
+import pages.{DescribeTheGoodsPage, NameOfGoodsPage}
 import repositories.SessionRepository
 import views.html.DescribeTheGoodsView
 
@@ -50,21 +47,25 @@ class DescribeTheGoodsController @Inject() (
 
   def onPageLoad(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData) {
     implicit request =>
+      val nameOfGoods = request.userAnswers.get(NameOfGoodsPage).getOrElse("No name of goods found")
+
       val preparedForm = request.userAnswers.get(DescribeTheGoodsPage) match {
         case None        => form
         case Some(value) => form.fill(value)
       }
 
-      Ok(view(preparedForm, mode))
+      Ok(view(nameOfGoods, preparedForm, mode))
   }
 
   def onSubmit(mode: Mode): Action[AnyContent] =
     (identify andThen getData andThen requireData).async {
       implicit request =>
+        val nameOfGoods = request.userAnswers.get(NameOfGoodsPage).getOrElse("No name of goods found")
+
         form
           .bindFromRequest()
           .fold(
-            formWithErrors => Future.successful(BadRequest(view(formWithErrors, mode))),
+            formWithErrors => Future.successful(BadRequest(view(nameOfGoods, formWithErrors, mode))),
             value =>
               for {
                 updatedAnswers <-
---
+++ app/viewmodels/govuk/TextareaFluency.scala
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewmodels.govuk
+
+import play.api.data.Field
+import play.api.i18n.Messages
+import uk.gov.hmrc.govukfrontend.views.viewmodels.hint.Hint
+import uk.gov.hmrc.govukfrontend.views.viewmodels.label.Label
+import uk.gov.hmrc.govukfrontend.views.viewmodels.textarea.Textarea
+import viewmodels.ErrorMessageAwareness
+
+object textarea extends TextareaFluency
+
+trait TextareaFluency {
+
+  object TextareaViewModel extends ErrorMessageAwareness {
+
+    def apply(
+      field: Field,
+      label: Label
+    )(implicit messages: Messages): Textarea =
+      Textarea(
+        id = field.id,
+        name = field.name,
+        value = field.value,
+        label = label,
+        errorMessage = errorMessage(field)
+      )
+  }
+
+  implicit class FluentTextarea(textarea: Textarea) {
+
+    def withId(id: String): Textarea =
+      textarea copy (id = id)
+
+    def describedBy(value: String): Textarea =
+      textarea copy (describedBy = Some(value))
+
+    def withHint(hint: Hint): Textarea =
+      textarea copy (hint = Some(hint))
+
+    def withFormGroupClasses(classes: String): Textarea =
+      textarea copy (formGroupClasses = classes)
+
+    def withCssClass(newClass: String): Textarea =
+      textarea copy (classes = s"${textarea.classes} $newClass")
+
+    def withAutocomplete(value: String): Textarea =
+      textarea copy (autocomplete = Some(value))
+
+    def withAttribute(attribute: (String, String)): Textarea =
+      textarea copy (attributes = textarea.attributes + attribute)
+
+    def withSpellcheck(on: Boolean = true): Textarea =
+      textarea copy (spellcheck = Some(on))
+
+    def withSize(size: String): Textarea =
+      textarea.withCssClass(size)
+  }
+}
