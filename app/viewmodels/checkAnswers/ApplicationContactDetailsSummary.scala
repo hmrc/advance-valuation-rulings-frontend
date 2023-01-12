1e19eb90a171007b25d43865a6c0eecc51484b2e
ymabdulahi <68248111+ymabdulahi@users.noreply.github.com>
2023-01-12 14:00:42 +0000

    minor fix to application details page

--- app/viewmodels/checkAnswers/ApplicationContactDetailsSummary.scala
+++ app/viewmodels/checkAnswers/ApplicationContactDetailsSummary.scala
@@ -33,8 +33,8 @@ object ApplicationContactDetailsSummary {
     answers.get(ApplicationContactDetailsPage).map {
       answer =>
         val value = HtmlFormat.escape(answer.name).toString + "<br/>" +
-          HtmlFormat.escape(answer.phone).toString + "<br/>" +
-          HtmlFormat.escape(answer.email).toString
+          HtmlFormat.escape(answer.email).toString + "<br/>" +
+          HtmlFormat.escape(answer.phone).toString
         SummaryListRowViewModel(
           key = "applicationContactDetails.checkYourAnswersLabel",
           value = ValueViewModel(HtmlContent(value)),
