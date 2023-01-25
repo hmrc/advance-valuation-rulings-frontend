b1639acf66a82427672c540bdd1aefd48863fb67
RawToast <8013000+RawToast@users.noreply.github.com>
2023-01-25 17:20:25 +0000

    ARS-455 Method page for valuations (#42)

--- app/models/ValuationMethod.scala
+++ app/models/ValuationMethod.scala
@@ -17,7 +17,10 @@
 package models
 
 import play.api.i18n.Messages
+import play.twirl.api.Html
 import uk.gov.hmrc.govukfrontend.views.Aliases.Text
+import uk.gov.hmrc.govukfrontend.views.viewmodels.content.HtmlContent
+import uk.gov.hmrc.govukfrontend.views.viewmodels.hint.Hint
 import uk.gov.hmrc.govukfrontend.views.viewmodels.radios.RadioItem
 
 sealed trait ValuationMethod
@@ -25,19 +28,35 @@ sealed trait ValuationMethod
 object ValuationMethod extends Enumerable.Implicits {
 
   case object Method1 extends WithName("method1") with ValuationMethod
+
   case object Method2 extends WithName("method2") with ValuationMethod
+
+  case object Method3 extends WithName("method3") with ValuationMethod
+
+  case object Method4 extends WithName("method4") with ValuationMethod
+
+  case object Method5 extends WithName("method5") with ValuationMethod
+
+  case object Method6 extends WithName("method6") with ValuationMethod
 
   val values: Seq[ValuationMethod] = Seq(
     Method1,
-    Method2
+    Method2,
+    Method3,
+    Method4,
+    Method5,
+    Method6
   )
 
   def options(implicit messages: Messages): Seq[RadioItem] = values.zipWithIndex.map {
     case (value, index) =>
       RadioItem(
-        content = Text(messages(s"valuationMethod.${value.toString}")),
+        content = HtmlContent(
+          Html("<strong>" + messages(s"valuationMethod.${value.toString}") + "</strong>")
+        ),
         value = Some(value.toString),
-        id = Some(s"value_$index")
+        id = Some(s"value_$index"),
+        hint = Some(Hint(content = Text(messages(s"valuationMethod.${value.toString}.hint"))))
       )
   }
 
