47b0c478d539088a533a111c7fd71ca600478273
RawToast <8013000+RawToast@users.noreply.github.com>
2023-02-20 10:11:06 +0000

    ARS-543 Method 1 Question Set 2 Scaffolds (#60)

--- test-utils/generators/UserAnswersGenerator.scala
+++ test-utils/generators/UserAnswersGenerator.scala
@@ -28,7 +28,11 @@ trait UserAnswersGenerator extends TryValues {
   self: Generators =>
 
   val generators: Seq[Gen[(QuestionPage[_], JsValue)]] =
-    arbitrary[(WhyTransactionValueOfSimilarGoodsPage.type, JsValue)] ::
+    arbitrary[(IsTheSaleSubjectToConditionsPage.type, JsValue)] ::
+      arbitrary[(DescribeTheRestrictionsPage.type, JsValue)] ::
+      arbitrary[(DescribeTheConditionsPage.type, JsValue)] ::
+      arbitrary[(AreThereRestrictionsOnTheGoodsPage.type, JsValue)] ::
+      arbitrary[(WhyTransactionValueOfSimilarGoodsPage.type, JsValue)] ::
       arbitrary[(HaveYouUsedMethodOneInPastPage.type, JsValue)] ::
       arbitrary[(WhyComputedValuePage.type, JsValue)] ::
       arbitrary[(WhyIdenticalGoodsPage.type, JsValue)] ::
