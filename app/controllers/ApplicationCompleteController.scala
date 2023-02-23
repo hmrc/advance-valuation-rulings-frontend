9c6122283b9bc36ee3a2916bd71e35a0946e4b06
James Morris <8013000+RawToast@users.noreply.github.com>
2023-02-23 15:05:38 +0000

    require data on application complete

--- app/controllers/ApplicationCompleteController.scala
+++ app/controllers/ApplicationCompleteController.scala
@@ -35,5 +35,7 @@ class ApplicationCompleteController @Inject() (
 ) extends FrontendBaseController
     with I18nSupport {
 
-  def onPageLoad: Action[AnyContent] = (identify andThen getData)(implicit request => Ok(view()))
+  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData) {
+    implicit request => Ok(view())
+  }
 }
