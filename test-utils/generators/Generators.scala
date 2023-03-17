97125f95aab74fed94b012539f0f83eb4cdbf17a
James Morris <8013000+RawToast@users.noreply.github.com>
2023-03-17 08:21:58 +0000

    Scaffolds for View Application & Ruling pages

--- test-utils/generators/Generators.scala
+++ test-utils/generators/Generators.scala
@@ -21,7 +21,6 @@ import java.time.{Instant, LocalDate, ZoneOffset}
 import org.scalacheck.{Gen, Shrink}
 import org.scalacheck.Arbitrary._
 import org.scalacheck.Gen._
-import wolfendale.scalacheck.regexp.RegexpGen
 
 trait Generators
     extends UserAnswersGenerator
