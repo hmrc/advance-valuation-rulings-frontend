808eeaa025a99da11dbeb0501d4fcd513c1f2981
RawToast <8013000+RawToast@users.noreply.github.com>
2023-05-04 15:02:12 +0100

    AuthUserType model (#272)

---
+++ app/models/AuthUserType.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models
+
+import uk.gov.hmrc.auth.core.{AffinityGroup, Assistant, CredentialRole, User}
+import uk.gov.hmrc.auth.core.Admin
+
+import enumeratum.{Enum, EnumEntry, PlayJsonEnum}
+
+sealed abstract class AuthUserType(override val entryName: String) extends EnumEntry
+
+object AuthUserType extends Enum[AuthUserType] with PlayJsonEnum[AuthUserType] {
+  val values: IndexedSeq[AuthUserType] = findValues
+
+  case object IndividualTrader extends AuthUserType("IndividualTrader")
+
+  case object OrganisationAdmin extends AuthUserType("OrganisationAdmin") // org + user/admin
+  case object OrganisationAssistant extends AuthUserType("OrganisationAssistant") // org + assistant
+
+  def apply(
+    affinityGroup: AffinityGroup,
+    credentialRole: Option[CredentialRole]
+  ): Option[AuthUserType] =
+    (affinityGroup, credentialRole) match {
+      case (AffinityGroup.Individual, _)                 => Some(IndividualTrader)
+      case (AffinityGroup.Organisation, Some(Assistant)) => Some(OrganisationAssistant)
+      case (AffinityGroup.Organisation, Some(Admin))     => Some(OrganisationAdmin)
+      case (AffinityGroup.Organisation, Some(User))      => Some(OrganisationAdmin)
+      case _                                             => None
+    }
+}
---
+++ test/models/AuthUserTypeSpec.scala
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models
+
+import play.api.libs.json._
+import uk.gov.hmrc.auth.core.{Admin, AffinityGroup, Assistant, User}
+
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.prop.TableDrivenPropertyChecks
+
+class AuthUserTypeSpec extends AnyFreeSpec with Matchers with TableDrivenPropertyChecks {
+
+  "AuthUserType" - {
+
+    "identifies an Invididual Trader" in {
+      val result = AuthUserType(AffinityGroup.Individual, None)
+
+      result mustBe Some(AuthUserType.IndividualTrader)
+    }
+
+    "identifies an Organisation Assistant" in {
+      val result = AuthUserType(AffinityGroup.Organisation, Some(Assistant))
+
+      result mustBe Some(AuthUserType.OrganisationAssistant)
+    }
+
+    "identifies an Organisation Admin with the Admin Credential Role" in {
+      val result = AuthUserType(AffinityGroup.Organisation, Some(Admin))
+
+      result mustBe Some(AuthUserType.OrganisationAdmin)
+    }
+
+    "identifies an Organisation Admin with the User Credential Role" in {
+      val result = AuthUserType(AffinityGroup.Organisation, Some(User))
+
+      result mustBe Some(AuthUserType.OrganisationAdmin)
+    }
+
+    val credentialRoles = Table("credentialRole", Admin, Assistant, User)
+    "returns None for unsupported `Agent` AffinityGroup" in {
+      forAll(credentialRoles) {
+        credentialRole =>
+          val result = AuthUserType(AffinityGroup.Agent, Some(credentialRole))
+
+          result mustBe None
+      }
+    }
+
+    "returns None for an Organisation without a CredentialRole" in {
+      val result = AuthUserType(AffinityGroup.Organisation, None)
+
+      result mustBe None
+    }
+
+    val scenarios = Table(
+      ("authUserType", "expectedJson"),
+      (AuthUserType.IndividualTrader, JsString("IndividualTrader")),
+      (AuthUserType.OrganisationAssistant, JsString("OrganisationAssistant")),
+      (AuthUserType.OrganisationAdmin, JsString("OrganisationAdmin"))
+    )
+
+    "serialises to Json" in {
+      forAll(scenarios) {
+        (authUserType, expectedJson) => Json.toJson(authUserType) mustEqual expectedJson
+      }
+    }
+
+    "deserialises from Json" in {
+      forAll(scenarios) {
+        (authUserType, expectedJson) =>
+          Json.fromJson[AuthUserType](expectedJson) mustEqual JsSuccess(authUserType)
+      }
+    }
+  }
+}
