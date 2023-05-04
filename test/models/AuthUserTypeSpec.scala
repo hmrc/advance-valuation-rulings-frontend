466aa6bf48e220c0909fd806f8b75ea1e74df33f
James Morris <8013000+RawToast@users.noreply.github.com>
2023-05-04 13:34:39 +0100

    fixup test

--- test/models/AuthUserTypeSpec.scala
+++ test/models/AuthUserTypeSpec.scala
@@ -16,12 +16,16 @@
 
 package models
 
+import play.api.libs.json.Json
+import play.api.libs.json.JsString
+import play.api.libs.json.JsSuccess
+import uk.gov.hmrc.auth.core.{Admin, AffinityGroup, Assistant, User}
+
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
-import uk.gov.hmrc.auth.core.AffinityGroup
-import uk.gov.hmrc.auth.core.{AffinityGroup, Assistant, CredentialRole, User}
+import org.scalatest.prop.TableDrivenPropertyChecks
 
-class AuthUserTypeSpec extends AnyFreeSpec with Matchers {
+class AuthUserTypeSpec extends AnyFreeSpec with Matchers with TableDrivenPropertyChecks {
 
   "AuthUserType" - {
 
@@ -31,22 +35,48 @@ class AuthUserTypeSpec extends AnyFreeSpec with Matchers {
       result mustBe Some(AuthUserType.IndividualTrader)
     }
 
-    "identifies an Organisation Admin" in {
+    "identifies an Organisation Assistant" in {
+      val result = AuthUserType(AffinityGroup.Organisation, Some(Assistant))
+
+      result mustBe Some(AuthUserType.OrganisationAssistant)
+    }
+
+    "identifies an Organisation Admin with the Admin Credential Role" in {
       val result = AuthUserType(AffinityGroup.Organisation, Some(Admin))
 
       result mustBe Some(AuthUserType.OrganisationAdmin)
     }
 
-    "identifies an Organisation User" in {
+    "identifies an Organisation Admin with the User Credential Role" in {
       val result = AuthUserType(AffinityGroup.Organisation, Some(User))
 
-      result mustBe Some(AuthUserType.OrganisationUser)
+      result mustBe Some(AuthUserType.OrganisationAdmin)
     }
 
     "identifies an Agent" in {
       val result = AuthUserType(AffinityGroup.Agent, None)
 
       result mustBe None
+    }
+
+    val scenarios = Table(
+      ("authUserType", "expectedJson"),
+      (AuthUserType.IndividualTrader, JsString("IndividualTrader")),
+      (AuthUserType.OrganisationAssistant, JsString("OrganisationAssistant")),
+      (AuthUserType.OrganisationAdmin, JsString("OrganisationAdmin"))
+    )
+
+    "serialises to Json" in {
+      forAll(scenarios) {
+        (authUserType, expectedJson) => Json.toJson(authUserType) mustEqual expectedJson
+      }
+    }
+
+    "deserialises from Json" in {
+      forAll(scenarios) {
+        (authUserType, expectedJson) =>
+          Json.fromJson[AuthUserType](expectedJson) mustEqual JsSuccess(authUserType)
+      }
     }
   }
 }
