79adf95da5c9752fb0be4980a6e00c1d38066276
Carl Thompson <carl.thompson@opencastsoftware.com>
2023-06-19 14:12:52 +0100

    fill content on page

--- app/controllers/DraftHasBeenSavedController.scala
+++ app/controllers/DraftHasBeenSavedController.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
 import javax.inject.Inject
@@ -12,14 +28,11 @@ import views.html.DraftHasBeenSavedView
 class DraftHasBeenSavedController @Inject() (
   override val messagesApi: MessagesApi,
   identify: IdentifierAction,
-  getData: DataRetrievalActionProvider,
-  requireData: DataRequiredAction,
   val controllerComponents: MessagesControllerComponents,
   view: DraftHasBeenSavedView
 ) extends FrontendBaseController
     with I18nSupport {
 
-  def onPageLoad: Action[AnyContent] = (identify andThen getData(draftId) andThen requireData) {
-    implicit request => Ok(view())
-  }
+  def onPageLoad(): Action[AnyContent] =
+    identify(implicit request => Ok(view()))
 }
