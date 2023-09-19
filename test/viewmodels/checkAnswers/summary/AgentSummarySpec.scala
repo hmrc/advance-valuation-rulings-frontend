81b1ace1c57d04bce36537def0debd45eb5026a2
Ewan-James-Donovan <34974028+Ewan-James-Donovan@users.noreply.github.com>
2023-09-19 15:39:12 +0100

    Update in line with ARSSTB-460

--- test/viewmodels/checkAnswers/summary/AgentSummarySpec.scala
+++ test/viewmodels/checkAnswers/summary/AgentSummarySpec.scala
@@ -69,7 +69,7 @@ class AgentSummarySpec extends SpecBase {
       val rows    = summary.rows.rows.map(row => (row.key, row.value))
 
       "must create rows for each page" in {
-        rows.length mustBe 6
+        rows.length mustBe 7
       }
 
       "create row for agent applicant name" in {
@@ -86,6 +86,19 @@ class AgentSummarySpec extends SpecBase {
           (
             Key(Text("checkYourAnswersForAgents.agent.org.email.label")),
             Value(Text(ContactEmail))
+          )
+        )
+      }
+
+      "create row for agent applicant role" in {
+        rows must contain(
+          (
+            Key(Text("checkYourAnswersForAgents.applicant.role.label")),
+            Value(
+              Text(
+                s"${WhatIsYourRoleAsImporter.MessagePrefix}.${WhatIsYourRoleAsImporter.AgentOnBehalfOfOrg}"
+              )
+            )
           )
         )
       }
