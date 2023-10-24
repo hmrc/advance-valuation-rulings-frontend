848ceb6f1c8a37c43f3856c535f9de4d75bd7d33
Ewan-James-Donovan <34974028+Ewan-James-Donovan@users.noreply.github.com>
2023-10-24 15:59:48 +0100

    welsh date fix v1

--- app/models/ApplicationForAccountHome.scala
+++ app/models/ApplicationForAccountHome.scala
@@ -18,6 +18,7 @@ package models
 
 import java.time.{Instant, ZoneId}
 import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 import play.api.i18n.Messages
 import play.api.mvc.Call
@@ -34,8 +35,12 @@ final case class ApplicationForAccountHome(
   statusTag: Tag,
   actions: Seq[ActionItem]
 ) {
-  def dateString(): String =
-    ApplicationForAccountHome.formatter.format(date)
+  def dateString(isWelsh: Boolean = false): String =
+    if (isWelsh) {
+      ApplicationForAccountHome.welshFormatter.format(date)
+    } else {
+      ApplicationForAccountHome.formatter.format(date)
+    }
 }
 
 object ApplicationForAccountHome {
@@ -44,6 +49,12 @@ object ApplicationForAccountHome {
     DateTimeFormatter
       .ofPattern("dd MMMM yyyy")
       .withZone(ZoneId.systemDefault())
+
+  private val welshFormatter =
+    DateTimeFormatter
+      .ofPattern("dd MMMM yyyy")
+      .withZone(ZoneId.systemDefault())
+      .withLocale(new Locale("cy", "GB"))
 
   def apply(applicationSummary: ApplicationSummary)(implicit
     messages: Messages
---
+++ worksheet.sc
@@ -0,0 +1,1 @@
+Gwirio’ch manylion EORI
