2823b387907baf533519f16da272da58110218c9
Gaston Racui <gastonracui@gmail.com>
2024-03-01 16:14:03 +0000

    DDCE-5244 Tudor Crown update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc"            % "7.29.0-play-28",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-28"    % "8.5.0",
     "uk.gov.hmrc"                  %% "play-conditional-form-mapping" % "1.13.0-play-28",
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28"    % bootstrapVersion,
     "uk.gov.hmrc"                  %% "internal-auth-client-play-28"  % "1.10.0",
