685195e8572212b75769078736789791f58b12b9
mywyau <47248104+mywyau@users.noreply.github.com>
2024-03-19 12:02:16 +0000

    remove redundant js for previous backlink implementation, to prevent double backs

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,19 +3,6 @@ if (window.history && window.history.replaceState && typeof window.history.repla
   window.history.replaceState(null, null, window.location.href);
 }
 
-document.addEventListener('DOMContentLoaded', function(event) {
-
-  // handle back click
-  var backLink = document.querySelector('.govuk-back-link');
-  if (backLink !== null) {
-    backLink.addEventListener('click', function(e){
-      e.preventDefault();
-      e.stopPropagation();
-      window.history.back();
-    });
-  }
-});
-
 // Find first ancestor of el with tagName
 // or undefined if not found
 function upTo(el, tagName) {
