e06e28f81f3b897aea4789d9d60686d83c140263
Taiwo-AB <63714619+Taiwo-AB@users.noreply.github.com>
2024-08-29 15:50:03 +0100

    DDCE-6001 Flakey test with static data (#597)

--- test/viewmodels/checkAnswers/DateSubmittedSummarySpec.scala
+++ test/viewmodels/checkAnswers/DateSubmittedSummarySpec.scala
@@ -17,18 +17,84 @@
 package viewmodels.checkAnswers
 
 import base.SpecBase
-import generators.ApplicationGenerator
-import models.requests.Application
+import models.requests._
 import play.api.i18n.{Lang, Messages}
 import play.api.test.Helpers.stubMessagesApi
 import viewmodels.govuk.summarylist._
 import viewmodels.implicits._
 
-import java.time.ZoneId
+import java.time.{Clock, Instant, ZoneId, ZoneOffset}
 import java.time.format.DateTimeFormatter
 import java.util.Locale
 
-class DateSubmittedSummarySpec extends SpecBase with ApplicationGenerator {
+class DateSubmittedSummarySpec extends SpecBase {
+
+  private val eoriDetails: TraderDetail = TraderDetail(
+    eori = "eori",
+    businessName = "business name",
+    addressLine1 = "address line 1",
+    addressLine2 = Some("address line 2"),
+    addressLine3 = None,
+    postcode = "postcode",
+    countryCode = "country code",
+    phoneNumber = None,
+    isPrivate = Some(true)
+  )
+
+  private val contact: ContactDetails = ContactDetails(
+    name = "contact name",
+    email = "email@example.com",
+    phone = Some("phone"),
+    companyName = Some("company name"),
+    jobTitle = Some("job title")
+  )
+
+  private val requestedMethod: MethodThree = MethodThree(
+    whyNotOtherMethods = "method 3 why not",
+    previousSimilarGoods = PreviousSimilarGoods("previous similar goods")
+  )
+
+  private val goodsDetails: GoodsDetails = GoodsDetails(
+    goodsDescription = "goods description",
+    envisagedCommodityCode = Some("commodity code"),
+    knownLegalProceedings = Some("legal"),
+    confidentialInformation = Some("confidential"),
+    similarRulingGoodsInfo = Some("goods info"),
+    similarRulingMethodInfo = Some("methods info")
+  )
+
+  private val lastUpdated: Instant = Instant.now(
+    Clock.fixed(Instant.parse("2018-08-22T10:00:00Z"), ZoneOffset.UTC)
+  )
+
+  private val applicationRequest: ApplicationRequest = ApplicationRequest(
+    draftId = draftId,
+    trader = eoriDetails,
+    agent = None,
+    contact = contact,
+    requestedMethod = requestedMethod,
+    goodsDetails = goodsDetails,
+    attachments = Nil,
+    whatIsYourRole = WhatIsYourRole.EmployeeOrg,
+    letterOfAuthority = None
+  )
+
+  private val applicationId: ApplicationId = ApplicationId(0L)
+
+  private val application: Application =
+    Application(
+      id = applicationId,
+      lastUpdated = lastUpdated,
+      created = lastUpdated,
+      trader = applicationRequest.trader,
+      agent = applicationRequest.agent,
+      contact = applicationRequest.contact,
+      requestedMethod = applicationRequest.requestedMethod,
+      goodsDetails = applicationRequest.goodsDetails,
+      attachments = Nil,
+      whatIsYourRoleResponse = Some(WhatIsYourRole.EmployeeOrg),
+      letterOfAuthority = None
+    )
 
   ".row" - {
 
@@ -39,8 +105,6 @@ class DateSubmittedSummarySpec extends SpecBase with ApplicationGenerator {
       s"must create row for DateSubmittedSummary and format the date using the appropriate locale when the language is $lang" in {
 
         implicit val messages: Messages = stubMessagesApi().preferred(Seq(Lang(lang)))
-
-        val application: Application = arbitraryApplication.arbitrary.sample.value
 
         val expectedFormattedDate: String = DateTimeFormatter
           .ofPattern("dd MMMM yyyy")
