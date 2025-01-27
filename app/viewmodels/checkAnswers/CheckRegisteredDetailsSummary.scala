0fb0f914b1a18bcf0ee1ecd018fbdd8a65d7a570
SachaMcCormack <175815565+SachaMcCormack@users.noreply.github.com>
2025-01-27 15:13:42 +0000

    DDCE-5237: copyright to 2025 (#604)
    * removed change button from registerdName and registeredAddress row of SummaryDetails
    
    * updated dependencies
    
    * updated plugins
    
    * copyright to 2025
    
    * remove migration plugin

--- app/viewmodels/checkAnswers/CheckRegisteredDetailsSummary.scala
+++ app/viewmodels/checkAnswers/CheckRegisteredDetailsSummary.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 HM Revenue & Customs
+ * Copyright 2025 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,11 +20,12 @@ import play.api.i18n.Messages
 import play.twirl.api.{Html, HtmlFormat}
 import uk.gov.hmrc.govukfrontend.views.viewmodels.content.HtmlContent
 import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryListRow
-
 import controllers.routes
 import models.{CheckMode, Country, DraftId, EoriNumber, TraderDetailsWithCountryCode}
-import viewmodels.govuk.summarylist._
-import viewmodels.implicits._
+import viewmodels.govuk.summarylist.*
+import viewmodels.implicits.*
+
+import scala.collection
 
 object CheckRegisteredDetailsSummary {
 
@@ -33,14 +34,7 @@ object CheckRegisteredDetailsSummary {
   ): SummaryListRow =
     SummaryListRowViewModel(
       key = "checkYourAnswers.eori.name.label",
-      value = ValueViewModel(HtmlFormat.escape(details.CDSFullName).body),
-      actions = Seq(
-        ActionItemViewModel(
-          "site.change",
-          routes.CheckRegisteredDetailsController.onPageLoad(CheckMode, draftId).url
-        )
-          .withVisuallyHiddenText(messages("checkRegisteredDetails.name.change.hidden"))
-      )
+      value = ValueViewModel(HtmlFormat.escape(details.CDSFullName).body)
     )
 
   private def registeredAddressRow(details: TraderDetailsWithCountryCode, draftId: DraftId)(implicit
@@ -61,13 +55,6 @@ object CheckRegisteredDetailsSummary {
               )
           )
         )
-      ),
-      actions = Seq(
-        ActionItemViewModel(
-          "site.change",
-          routes.CheckRegisteredDetailsController.onPageLoad(CheckMode, draftId).url
-        )
-          .withVisuallyHiddenText(messages("checkRegisteredDetails.address.change.hidden"))
       )
     )
 
