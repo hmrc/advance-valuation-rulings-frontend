6f88fb333c841a80c22d4ddd5ce4488276bb4d8c
Lorenzo Mugnai <11977337+lmugnai@users.noreply.github.com>
2025-07-15 13:40:11 +0100

    Add new gov.uk rebranding (#624)
    * Update deps + add govuk rebranding toggle
    
    * Update sbt plugin

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,10 +3,10 @@ import sbt.*
 object AppDependencies {
 
   private val hmrcMongoVersion = "2.6.0"
-  private val bootstrapVersion = "9.13.0"
+  private val bootstrapVersion = "9.16.0"
 
   private val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30"            % "12.6.0",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30"            % "12.7.0",
     "uk.gov.hmrc"                  %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc"                  %% "internal-auth-client-play-30"          % "4.0.0",
@@ -14,7 +14,7 @@ object AppDependencies {
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,
     "org.typelevel"                %% "cats-core"                             % "2.13.0",
     "com.beachape"                 %% "enumeratum-play-json"                  % "1.9.0",
-    "com.googlecode.libphonenumber" % "libphonenumber"                        % "9.0.8"
+    "com.googlecode.libphonenumber" % "libphonenumber"                        % "9.0.9"
   )
 
   private val test: Seq[ModuleID] = Seq(
