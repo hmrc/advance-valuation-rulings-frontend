7f411d6d5cc78d8645eead98a7a7ba07e84fdc6a
tolgatunaaa <107684014+tolgatunaaa@users.noreply.github.com>
2026-01-08 15:01:03 +0000

    DDCE-7959 - use the new GOV.UK service navigation

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,11 +2,11 @@ import sbt.*
 
 object AppDependencies {
 
-  private val hmrcMongoVersion = "2.6.0"
-  private val bootstrapVersion = "9.16.0"
+  private val hmrcMongoVersion = "2.7.0"
+  private val bootstrapVersion = "9.18.0"
 
   private val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30"            % "12.7.0",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30"            % "12.16.0",
     "uk.gov.hmrc"                  %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc"                  %% "internal-auth-client-play-30"          % "4.0.0",
