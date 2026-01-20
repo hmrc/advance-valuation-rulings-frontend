70a3fb319a06d50763ac55a6f6c148f0ed66379f
Tolga TUNA <107684014+tolgatunaaa@users.noreply.github.com>
2026-01-20 07:53:41 +0000

    DDCE-7959 - use the new GOV.UK service navigation (#625)
    * DDCE-7959 - use the new GOV.UK service navigation
    
    * DDCE-7959 - Upgrade versions
    
    * DDCE-7959 - More upgrade versions
    
    * DDCE-7959 - Test fix
    
    * DDCE-7959 - More test fix
    
    * DDCE-7959 - FileService contentSha256 fix for new version!
    
    * DDCE-7959 - FileServiceSpec fix!
    
    * Copyright 2025 to 2026 (#626)
    
    * DDCE-7959 - Revert file upload changes

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,14 +2,14 @@ import sbt.*
 
 object AppDependencies {
 
-  private val hmrcMongoVersion = "2.6.0"
-  private val bootstrapVersion = "9.16.0"
+  private val hmrcMongoVersion = "2.11.0"
+  private val bootstrapVersion = "9.19.0"
 
   private val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30"            % "12.7.0",
-    "uk.gov.hmrc"                  %% "play-conditional-form-mapping-play-30" % "3.3.0",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30"            % "12.27.0",
+    "uk.gov.hmrc"                  %% "play-conditional-form-mapping-play-30" % "3.4.0",
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-30"            % bootstrapVersion,
-    "uk.gov.hmrc"                  %% "internal-auth-client-play-30"          % "4.0.0",
+    "uk.gov.hmrc"                  %% "internal-auth-client-play-30"          % "4.3.0",
     "uk.gov.hmrc.objectstore"      %% "object-store-client-play-30"           % "2.2.0",
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,
     "org.typelevel"                %% "cats-core"                             % "2.13.0",
--- test/views/behaviours/ViewBehaviours.scala
+++ test/views/behaviours/ViewBehaviours.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 HM Revenue & Customs
+ * Copyright 2026 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,7 +35,7 @@ trait ViewBehaviours extends ViewSpecBase {
 
       "and have the correct banner title" in {
         val doc         = asDocument(viewViaApply)
-        val bannerTitle = doc.getElementsByClass("govuk-header__service-name")
+        val bannerTitle = doc.getElementsByClass("govuk-service-navigation__service-name")
         bannerTitle.text mustBe messages("service.name")
       }
     }
