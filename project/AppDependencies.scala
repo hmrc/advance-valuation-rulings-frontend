b5f283089d6e267a191ab752b4b77c66b65c8101
Suraj Suseelan <186935751+suraj-suseelan@users.noreply.github.com>
2026-04-29 07:01:09 +0100

    DDCE-8468: AVR: advance-valuation-rulings-frontend Security Vulnerabilities CVE-2025-66566 (#629)
    * DDCE-8468: AVR: advance-valuation-rulings-frontend Security Vulnerabilities CVE-2025-66566
    
    * DDCE-8468: AVR: advance-valuation-rulings-frontend Security Vulnerabilities CVE-2025-66566
    
    * DDCE-8468: AVR: advance-valuation-rulings-frontend Security Vulnerabilities CVE-2025-66566

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,19 +2,19 @@ import sbt.*
 
 object AppDependencies {
 
-  private val hmrcMongoVersion = "2.11.0"
-  private val bootstrapVersion = "9.19.0"
+  private val hmrcMongoVersion = "2.12.0"
+  private val bootstrapVersion = "10.7.0"
 
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30"            % "12.27.0",
-    "uk.gov.hmrc"                  %% "play-conditional-form-mapping-play-30" % "3.4.0",
+    "uk.gov.hmrc"                  %% "play-conditional-form-mapping-play-30" % "3.5.0",
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc"                  %% "internal-auth-client-play-30"          % "4.3.0",
     "uk.gov.hmrc.objectstore"      %% "object-store-client-play-30"           % "2.2.0",
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,
     "org.typelevel"                %% "cats-core"                             % "2.13.0",
-    "com.beachape"                 %% "enumeratum-play-json"                  % "1.9.0",
-    "com.googlecode.libphonenumber" % "libphonenumber"                        % "9.0.9"
+    "com.beachape"                 %% "enumeratum-play-json"                  % "1.9.7",
+    "com.googlecode.libphonenumber" % "libphonenumber"                        % "9.0.28"
   )
 
   private val test: Seq[ModuleID] = Seq(
