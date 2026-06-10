4cd288fbf47f7e9e7128d2ef25d4d41d82c93628
suraj-suseelan <186935751+suraj-suseelan@users.noreply.github.com>
2026-06-10 12:41:22 +0100

    DDCE-8561: AVR: advance-valuation-rulings-frontend play-frontend upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
     "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30"            % "13.8.0",
     "uk.gov.hmrc"                  %% "play-conditional-form-mapping-play-30" % "3.5.0",
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-30"            % bootstrapVersion,
-    "uk.gov.hmrc"                  %% "internal-auth-client-play-30"          % "4.3.0",
+    "uk.gov.hmrc"                  %% "internal-auth-client-play-30"          % "4.4.0",
     "uk.gov.hmrc.objectstore"      %% "object-store-client-play-30"           % "2.6.0",
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,
     "org.typelevel"                %% "cats-core"                             % "2.13.0",
