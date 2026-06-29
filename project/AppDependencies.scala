0f2c15f5641ef25e03121711230df8bd56cae7f5
baznez <nesbeth@live.co.uk>
2026-06-29 17:07:38 +0100

    updated AppDependencies.scala

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object AppDependencies {
 
   private val hmrcMongoVersion = "2.12.0"
-  private val bootstrapVersion = "10.7.0"
+  private val bootstrapVersion = "10.8.0"
 
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30"            % "13.9.0",
@@ -13,8 +13,8 @@ object AppDependencies {
     "uk.gov.hmrc.objectstore"      %% "object-store-client-play-30"           % "2.6.0",
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,
     "org.typelevel"                %% "cats-core"                             % "2.13.0",
-    "com.beachape"                 %% "enumeratum-play-json"                  % "1.9.7",
-    "com.googlecode.libphonenumber" % "libphonenumber"                        % "9.0.32"
+    "com.beachape"                 %% "enumeratum-play-json"                  % "1.9.8",
+    "com.googlecode.libphonenumber" % "libphonenumber"                        % "9.0.33"
   )
 
   private val test: Seq[ModuleID] = Seq(
